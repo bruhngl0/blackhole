1b77cd687a8cc7db10a8736a57a7a218df29afcd
bruh.ngl0 <adityashrm500@gmail.com>
2024-01-13 12:58:52 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,17 @@ function App() {
   return (
     <>
     <div className='middle'>
-       
+       <div className='para-one'>
+         <p>look book</p>
+       </div>
+
+       <div className='logo'>
+         <img src="blackhole.png"  className='logo-img'/>
+       </div>
+
+       <div className='para-two'>
+          <p>web shop</p>
+       </div>
     </div>
     </>
   )
