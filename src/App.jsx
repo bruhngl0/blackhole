d71fc8f183f9a3ba07f814bee30f852124d2db89
bruh.ngl0 <adityashrm500@gmail.com>
2024-01-15 12:25:56 +0530

    balls

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ function App() {
 
        <img src= "ball1.png"  className='ball1' alt="ball"/>
        <img src= "ball2.png"  className='ball2'/>
+       <img src= "ball3.png"  className='ball3'/>
+       <img src= "ball1.png"  className='ball4'/>
+       <img src= "ball2.png"  className='ball5'/>
        
     </div>
     </>
