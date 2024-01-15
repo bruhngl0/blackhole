8205d7ed98011e5295808643a83817260f959de7
bruh.ngl0 <adityashrm500@gmail.com>
2024-01-15 17:53:55 +0530

    switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import Hero from './components/Hero';
 import Landing from './components/Landing';
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/products" element={<Hero />} />
-        </Routes>
-      </div>
+      <Switch>
+        <Route path="/" element={<Landing />} />
+        <Route path="/main" element={<Hero />} />
+      </Switch>
     </Router>
   );
 };
 
 export default App;
+
 
