065773f8be60d439b7c624720d119aa4267dedb5
bruh.ngl0 <adityashrm500@gmail.com>
2024-01-16 13:22:43 +0530

    kk

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,32 +6,48 @@ const Hero = () => {
     <>
     <div className='hero'>
      <div className='hero-one'>
-       <img src="wh.png" />
-       <img src="bkhood.png" />
+       <img src="./products/j1.png" />
+       <img src="./products/j2.png" />
      </div>
      
      <div className='hero-two'>
-       <img src="wh.png" />
-       <img src="bkhood.png" />
+       <img src="./products/Puff2.png" />
+       <img src="./products/Puff1.png" />
      </div>
     </div>
 
     <div className='page-one'>
       <div className='page-one-one'>
-      <img src="wh.png" />
-       <img src="bkhood.png" />
+      <img src="./products/Puff3.png" />
+       <img src="./products/VEST3.png" />
       </div>
 
       <div className='page-one-one'>
-      <img src="wh.png" />
-       <img src="bkhood.png" />
+      <img src="./products/VEST4.png" />
+       <img src="./products/VEST1.png" />
       </div>
 
       <div className='page-one-one'>
-      <img src="wh.png" />
-       <img src="bkhood.png" />
+      <img src="./products/VEST2.png" />
+       <img src="./products/Puffer6.png" />
+      </div>
+    </div>
+
+    <div className='page-one'>
+      <div className='page-one-one'>
+      <img src="./products/Puffer3.png" />
+       <img src="./products/Puffer2.png" />
       </div>
 
+      <div className='page-one-one'>
+      <img src="./products/Puffer5.png" />
+       <img src="./products/Puffer1.png" />
+      </div>
+
+      <div className='page-one-one'>
+      <img src="./products/Mesh_hoodie.png" />
+       <img src="./products/Mesh_hoodie_b.png" />
+      </div>
     </div>
     </>
   )
