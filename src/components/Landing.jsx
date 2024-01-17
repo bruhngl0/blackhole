e51c2245a90fd2d62af81794827c165d00f27450
bruh.ngl0 <adityashrm500@gmail.com>
2024-01-17 13:58:27 +0530

    text-less

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
     <>
     <div className='middle'>
        <div className='para-one'>
-         <p className='para-one-text'>. LOOKBOOK . 2024 WINTER FALL COLLECTION . LOOKBOOK . 2024 WINTER FALL COLLECTION . LOOKBOOK . 2024 WINTER FALL COLLECTION . LOOKBOOK . 2024 WINTER FALL COLLECTION . LOOKBOOK . 2024 WINTER FALL COLLECTION . LOOKBOOK . 2024 WINTER FALL COLLECTION</p>
+         <p className='para-one-text'>. LOOKBOOK . 2024 WINTER FALL COLLECTION . LOOKBOOK . 2024 WINTER FALL COLLECTION . </p>
        </div>
 
        <div className='logo'>
@@ -20,7 +20,7 @@ const Landing = () => {
        <Link to= "/products" className='link' >
        <div className='para-two'>
         
-          <p className='para-one-text'>. WEB SHOP . 2024 WINTER FALL COLLECTION . WEB SHOP . 2024 WINTER FALL COLLECTION WEB SHOP . 2024 WINTER FALL COLLECTION . WEB SHOP . 2024 WINTER FALL COLLECTION . WEB SHOP . 2024 WINTER FALL COLLECTION WEB SHOP . 2024 WINTER FALL COLLECTION</p>
+          <p className='para-one-text'>. WEB SHOP . 2024 WINTER FALL COLLECTION . WEB SHOP . 2024 WINTER FALL </p>
              
        </div>
        </Link> 
